944f97b43cdc1c58dd96d2175361cde9c16f4f0e
sharozraees802 <sharozraees802@gmail.com>
2022-11-19 13:47:38 +0500

    add a puppeteer code

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 const express = require('express');
-
+const puppeteer = require('puppeteer');
 const app = express();
 const port = 3000;
 
-// ?website=https://something.com
-app.get('/crawl',(req, res, next)=>{
+// http://localhost:3000/?website=https://something.com
+app.get('/crawl', async (req, res, next)=>{
     if (!req.query.website) {
         const err = new Error('Required Query website is missing');
         err.status = 400;
         next(err);
     }
     try{
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
+        await page.goto(req.query.website);
+        const html = await page.content();
+
+        await page.close();
+
+        // res.send(html);
+        return res.status(200).send(html);
     }
     catch(err){
         console.log(err);
@@ -21,4 +30,5 @@ app.get('/crawl',(req, res, next)=>{
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`click me http://localhost:3000/crawl?website=http://quotes.toscrape.com/`);
 })
